d07edd2e9ce72c9bf649727dea4f090ed27d3854
ForgeBuilder <elliotkarlson@gmail.com>
2026-04-07 15:02:18 -0500

    Rotate

---
+++ main_control.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@export var PositionNodes : Array[Control]
+var Positions : Array[Vector2]
+
+@export var PlayerLabels : Array[Node]
+
+var rotation_offset = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for position_node in PositionNodes:
+		Positions.append(position_node.position)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_rotate_button_down() -> void:
+	rotation_offset+=1
+	
+	var i = 0
+	for player_label : Node in PlayerLabels:
+		player_label.reparent(PositionNodes[(rotation_offset+i)%6])
+		i+=1
