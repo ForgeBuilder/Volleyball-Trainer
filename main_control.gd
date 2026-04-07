e58106772ad2113ce615dda49eaba622e0cbfdaa
ForgeBuilder <elliotkarlson@gmail.com>
2026-04-07 15:25:58 -0500

    Messing with position
    my choice to try and make this all via UI is coming back to bite me. I might want to make the court itself it's own veiwport so I can just stretch that.

--- main_control.gd
+++ main_control.gd
@@ -1,16 +1,15 @@
 extends Control
 
 @export var PositionNodes : Array[Control]
-var Positions : Array[Vector2]
-
+#var Positions : Array[Vector2
 @export var PlayerLabels : Array[Node]
 
 var rotation_offset = 0
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	for position_node in PositionNodes:
-		Positions.append(position_node.position)
+#func _ready() -> void:
+	#for position_node in PositionNodes:
+		#Positions.append(position_node.position)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -20,6 +19,7 @@ func _on_rotate_button_down() -> void:
 	rotation_offset+=1
 	
 	var i = 0
-	for player_label : Node in PlayerLabels:
+	for player_label : Control in PlayerLabels:
 		player_label.reparent(PositionNodes[(rotation_offset+i)%6])
+		#player_label.global_position = PositionNodes[(rotation_offset+i)%6].global_position
 		i+=1
